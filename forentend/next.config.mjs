feb9bc9084d6b161753caabf82695e5de8eef860
Magacaaga <you@example.com>
2026-03-01 11:12:31 +0300

    first commit

--- forentend/next.config.mjs
+++ forentend/next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*',
+        destination: 'https://siu-assets.onrender.com/api/:path*',
       },
     ]
   },
